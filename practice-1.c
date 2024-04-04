7150294bfd10d8c5b4b8e16fcb809202f1cc3e45
Habibur Rahaman <habibur@quantummethod.org.bd>
2024-04-04 16:04:43 +0600

    Problem solved of module 2.5 practice day-1

---
+++ practice-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Recently I heard that you’ve achieved\n95%% marks in your exam.\nThis is brilliant! I wish you’ll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
+
+    return 0;
+}
